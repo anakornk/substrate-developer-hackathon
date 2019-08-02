f9a33f084fc4e10919c87cd1d400ff26ae09c1ee
Anakorn Kyavatanakij <anakornk@gmail.com>
2019-08-02 23:00:52 +0700

    add cennznet api support

--- syloUI/App.js
+++ syloUI/App.js
@@ -8,6 +8,7 @@
 
 import React, {Component} from 'react';
 import {Platform, StyleSheet, Text, View} from 'react-native';
+import { Api, WsProvider } from "@cennznet/api";
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
@@ -18,10 +19,36 @@ const instructions = Platform.select({
 
 type Props = {};
 export default class App extends Component<Props> {
+  constructor(props) {
+    super(props);
+    this.state = {
+      text: '0'
+    }
+        // Initialise the provider to connect to the local node
+    const providerUrl = Platform.select({
+      ios: "ws://127.0.0.1:9944",
+      android: "ws://10.0.2.2:9944"
+    });
+    this.provider = new WsProvider(providerUrl);
+  }
+
+  async componentDidMount() {
+    this.api = await Api.create({
+      provider: this.provider
+    });
+    this.api.rpc.chain.subscribeNewHead(header => {
+      this.setState({
+        text: `Chain is at #${header.blockNumber}`
+      });
+    });
+  }
+  
   render() {
+    
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>Welcome to Sylo Connected App</Text>
+        <Text>{this.state.text}</Text>
       </View>
     );
   }
--- syloUI/metro.config.js
+++ syloUI/metro.config.js
@@ -5,13 +5,20 @@
  * @format
  */
 
+const nodeLibs = require("node-libs-react-native");
+nodeLibs.bs58 = require.resolve("bs58");
+nodeLibs.vm = require.resolve("vm-browserify");
+
 module.exports = {
   transformer: {
     getTransformOptions: async () => ({
       transform: {
         experimentalImportSupport: false,
-        inlineRequires: false,
-      },
-    }),
+        inlineRequires: false
+      }
+    })
   },
+  resolver: {
+    extraNodeModules: nodeLibs
+  }
 };
