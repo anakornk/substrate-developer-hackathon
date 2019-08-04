e9823ad51164a891398f1a15b3d0d6018014d030
Anakorn Kyavatanakij <anakornk@gmail.com>
2019-08-04 17:05:07 +0700

    clean up code

--- syloUI/App.js
+++ syloUI/App.js
@@ -50,56 +50,15 @@ const AppContainer = createAppContainer(DrawerNavigator);
 export default class App extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      text: '0'
-    }
-        // Initialise the provider to connect to the local node
+    
     this.providerUrl = Platform.select({
       ios: "ws://localhost:9944",
       android: "ws://10.0.2.2:9944"
     });
     
-    // this.providerUrl = new WsProvider(providerUrl);
   }
 
   render() {
     return <AppContainer screenProps={{providerUrl: this.providerUrl}}/>;
   }
 }
-// type Props = {};
-// export default class App extends Component<Props> {
-//   constructor(props) {
-//     super(props);
-//     this.state = {
-//       text: '0'
-//     }
-//         // Initialise the provider to connect to the local node
-//     const providerUrl = Platform.select({
-//       ios: "wss://cennznet-node-0.centrality.cloud:9944",
-//       android: "wss://cennznet-node-0.centrality.cloud:9944"
-//     });
-//     this.provider = new WsProvider(providerUrl);
-//   }
-
-//   async componentDidMount() {
-//     this.api = await Api.create({
-//       provider: this.provider
-//     });
-//     this.api.rpc.chain.subscribeNewHead(header => {
-//       this.setState({
-//         text: `Chain is at #${header.blockNumber}`
-//       });
-//     });
-//   }
-  
-//   render() {
-    
-//     return (
-//       <View style={styles.container}>
-//         <Text style={styles.welcome}>Welcome to Sylo Connected App</Text>
-//         <Text>{this.state.text}</Text>
-//       </View>
-//     );
-//   }
-// }
-
